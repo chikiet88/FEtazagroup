4c65a678f83d7bf15e22380e999dd70b82cfcbd5
kataprodev <chikiet88@gmail.com>
2022-07-18 02:14:42 +0700

    Update

--- src/app/modules/admin/thongtincoban/congviec/timeline/timeline.component.ts
+++ src/app/modules/admin/thongtincoban/congviec/timeline/timeline.component.ts
@@ -93,30 +93,40 @@ export class TimelineComponent implements OnInit {
     };
   }
 
-  daysToMilliseconds(days) {
-    return days * 24 * 60 * 60 * 1000;
-  }
-
-  cols = ["Task ID","Task Name","Start Date", 'End Date', 'Duration', 'Percent Complete', 'Dependencies'];
+  cols = ["Task ID","Task Name",'Resource',"Start Date", 'End Date', 'Duration', 'Percent Complete', 'Dependencies'];
   title = '';
   type = 'Gantt';
   data = [
-    ['Research', 'Find sources',
-     new Date(2015, 0, 1), new Date(2015, 0, 5), null,  100,  null],
-    ['Write', 'Write paper',
-     null, new Date(2015, 0, 9), this.daysToMilliseconds(3), 25, 'Research,Outline'],
-    ['Cite', 'Create bibliography',
-     null, new Date(2015, 0, 7), this.daysToMilliseconds(1), 20, 'Research'],
-    ['Complete', 'Hand in paper',
-     null, new Date(2015, 0, 10), this.daysToMilliseconds(1), 0, 'Cite,Write'],
-    ['Outline', 'Outline paper',
-     null, new Date(2015, 0, 6), this.daysToMilliseconds(1), 100, 'Research']
+    ['2014Spring', 'Spring 2014', 'spring',
+     new Date(2014, 2, 22), new Date(2014, 5, 20), 0, 100, null],
+    ['2014Summer', 'Summer 2014', 'summer',
+     new Date(2014, 5, 21), new Date(2014, 8, 20), 0, 100, null],
+    ['2014Autumn', 'Autumn 2014', 'autumn',
+     new Date(2014, 8, 21), new Date(2014, 11, 20), 0, 100, null],
+    ['2014Winter', 'Winter 2014', 'winter',
+     new Date(2014, 11, 21), new Date(2015, 2, 21), 0, 100, null],
+    ['2015Spring', 'Spring 2015', 'spring',
+     new Date(2015, 2, 22), new Date(2015, 5, 20), 0, 50, null],
+    ['2015Summer', 'Summer 2015', 'summer',
+     new Date(2015, 5, 21), new Date(2015, 8, 20), 0, 0, null],
+    ['2015Autumn', 'Autumn 2015', 'autumn',
+     new Date(2015, 8, 21), new Date(2015, 11, 20), 0, 0, null],
+    ['2015Winter', 'Winter 2015', 'winter',
+     new Date(2015, 11, 21), new Date(2016, 2, 21), 0, 0, null],
+    ['Football', 'Football Season', 'sports',
+     new Date(2014, 8, 4), new Date(2015, 1, 1), 0, 100, null],
+    ['Baseball', 'Baseball Season', 'sports',
+     new Date(2015, 2, 31), new Date(2015, 9, 20), 0, 14, null],
+    ['Basketball', 'Basketball Season', 'sports',
+     new Date(2014, 9, 28), new Date(2015, 5, 20), 0, 86, null],
+    ['Hockey', 'Hockey Season', 'sports',
+     new Date(2014, 9, 8), new Date(2015, 5, 21), 0, 89, null]
   ];
   options = {
     height: 400,
     gantt: {
       trackHeight: 30
-   }
+    }
   }
   ngOnInit(): void {
     
